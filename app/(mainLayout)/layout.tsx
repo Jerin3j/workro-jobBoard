eb718ca9e349b42922d3219d8f0c3991759569c8
Jerin3J <jerinjj202@gmail.com>
2025-08-23 23:37:20 +0530

    fix: onboardingCompleted is false, forcefully redirect to onboarding page

--- app/(mainLayout)/layout.tsx
+++ app/(mainLayout)/layout.tsx
@@ -1,7 +1,23 @@
 import { Navbar } from '@/components/layouts/Navbar'
 import React from 'react'
+import { auth } from '../utils/auth';
+import { prisma } from '../utils/db';
+import { redirect } from 'next/navigation';
 
-export default function layout  ({children}: {children : React.ReactNode}) {
+export default async function layout  ({children}: {children : React.ReactNode}) {
+  const session = await auth();
+
+  if (session?.user?.id) {
+    const user = await prisma.user.findUnique({
+      where: { id: session.user.id },
+      select: { onBoardingCompleted: true },
+    });
+
+    if (user && !user.onBoardingCompleted) {
+      redirect("/onboarding");
+    }
+  }
+
   return (
     <div className="w-full md:max-w-7xl mx-auto px-4 md:px-6 lg:px-8 pb-8 md:pb-12">
         <Navbar/>
