908ab20409b930c47767e7b362141690e7814854
Jerin3J <jerinjj202@gmail.com>
2025-08-23 13:01:10 +0530

    refact: salary sange selector slider

--- components/layouts/SalaryRangeSelector.tsx
+++ components/layouts/SalaryRangeSelector.tsx
@@ -11,37 +11,45 @@ interface SalaryRangeProps {
     currency: string;
 }
 
-export const SalaryRangeSelector = ({control, maxSalary, minSalary} : SalaryRangeProps) => {
-
-    const { field: fromFiled} = useController({
+export const SalaryRangeSelector = ({ control, maxSalary, minSalary, step }: SalaryRangeProps) => {
+    const { field: fromFiled } = useController({
         name: 'salaryFrom',
         control,
     })
 
-    const { field: toFiled} = useController({
+    const { field: toFiled } = useController({
         name: 'salaryTo',
         control,
     })
 
-    const  [range, setRange] = useState<[number, number]>([
+    const [range, setRange] = useState<[number, number]>([
         fromFiled.value || minSalary,
         toFiled.value || maxSalary / 2
     ])
 
     const handleChangeRange = (value: number[]) => {
-         const newRange : [number, number] = [value[0], value[1]];
-         setRange(newRange);
-         fromFiled.onChange(newRange[0]);
-         toFiled.onChange(newRange[1]);
+        // Snap each value to nearest step
+        const snap = (val: number) => Math.round(val / step) * step;
+        const newRange: [number, number] = [snap(value[0]), snap(value[1])];
+
+        setRange(newRange);
+        fromFiled.onChange(newRange[0]);
+        toFiled.onChange(newRange[1]);
     }
 
     return (
-    <div className='w-full space-y-4'>
-        <Slider onValueChange={handleChangeRange} min={minSalary} max={maxSalary} step={1} value={range} />
-    <div className="flex justify-between">
-        <span>{formatCurrency(range[0])}</span>
-        <span>{formatCurrency(range[1])}</span>
-    </div>
-    </div>
-  )
+        <div className='w-full space-y-4'>
+            <Slider 
+                onValueChange={handleChangeRange} 
+                min={minSalary} 
+                max={maxSalary} 
+                step={step} 
+                value={range} 
+            />
+            <div className="flex justify-between">
+                <span>{formatCurrency(range[0])}</span>
+                <span>{formatCurrency(range[1])}</span>
+            </div>
+        </div>
+    )
 }
